057dc6ec915b8515286b1d021683711283314f3e
Geovanna Otoni <geovannaotoni@gmail.com>
2023-09-25 15:03:51 -0300

    feat01: migration e model para a tabela de times

---
+++ app/backend/src/database/models/SequelizeTeam.ts
@@ -0,0 +1,35 @@
+import {
+  DataTypes,
+  Model,
+  InferAttributes,
+  InferCreationAttributes,
+  CreationOptional,
+} from 'sequelize';
+import db from '.';
+
+class SequelizeTeam extends Model<InferAttributes<SequelizeTeam>,
+InferCreationAttributes<SequelizeTeam>> {
+  declare id: CreationOptional<number>;
+
+  declare teamName: string;
+}
+
+SequelizeTeam.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  teamName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    field: 'team_name',
+  },
+}, {
+  sequelize: db,
+  modelName: 'teams',
+  timestamps: false,
+});
+
+export default SequelizeTeam;
